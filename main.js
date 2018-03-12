2a1f025631252b8660edeb3d11a8ba9731c62b87
Sahil0071 <sahilraja242@gmail.com>
2018-03-12 17:26:38 +0530

    Fourth

---
+++ main.js
@@ -0,0 +1,38 @@
+var p1 = document.querySelector("#player1");
+var p2 = document.getElementById("player2");
+var reset = document.querySelector("#reset");
+var p1Display = document.getElementById("pldisplay")
+var p2Display = document.getElementById("p2display")
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+p1.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+p2.addEventListener("click", function() {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+reset.addEventListener("click", function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+});
